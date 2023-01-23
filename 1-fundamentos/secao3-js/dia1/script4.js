f6fa8d00485e3a2005b10355871900b015d03194
Adryelle Felippe <adryellefelippe@outlook.com>
2023-01-23 16:11:35 -0300

    mais exercicios

---
+++ 1-fundamentos/secao3-js/dia1/script4.js
@@ -0,0 +1,8 @@
+const a = 4;
+const b = 8;
+
+console.log('Adição: ', a+b);
+console.log('Subtração: ', a-b);
+console.log('Multiplicação: ', a*b);
+console.log('Divisão: ', a/b);
+console.log('Módulo: ', a%b);
---
+++ 1-fundamentos/secao3-js/dia1/script5.js
@@ -0,0 +1,20 @@
+const a = 56;
+const b = 34;
+
+if(a>b){
+    console.log('O maior valor é: ', a);
+} else{
+    console.log('O maior valor é: ', b);
+}
+
+const c = 100;
+const d = 77;
+const e = 234;
+
+if(c>d && c>e){
+    console.log('O maior valor é: ', c);
+} else if(d>c && d>e){
+    console.log('O maior valor é: ', d);
+} else{
+    console.log('O maior valor é: ', e);
+}
