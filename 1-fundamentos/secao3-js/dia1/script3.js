e546eef04cac7f9b6288d24e61f09564c49d7054
Adryelle Felippe <adryellefelippe@outlook.com>
2023-01-23 15:59:53 -0300

    exercicio 4

---
+++ 1-fundamentos/secao3-js/dia1/script3.js
@@ -0,0 +1,16 @@
+let statusCandidato = 'aprovada';
+
+switch(statusCandidato){
+    case 'aprovada':
+        console.log('Parabéns, você foi aprovada(o)!');
+        break;
+    case 'aprovada':
+        console.log('Você está na nossa lista de espera');
+        break;
+    case 'aprovada':
+        console.log('Você foi reprovada(o)');
+        break;
+
+    default:
+        console.log('Informação incorreta');
+}
